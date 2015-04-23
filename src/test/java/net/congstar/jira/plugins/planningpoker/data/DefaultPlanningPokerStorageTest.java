25112d33f18ba2b2834d016742e13b5e095ceaf7
Stefan Glase <stefan.glase@googlemail.com>
2015-04-23 21:12:50 +0200

    implement visibility of decks

---
+++ src/test/java/net/congstar/jira/plugins/planningpoker/data/DefaultPlanningPokerStorageTest.java
@@ -0,0 +1,50 @@
+package net.congstar.jira.plugins.planningpoker.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DefaultPlanningPokerStorageTest {
+
+    private static final String ISSUE_ONE = "ISSUE-1";
+    private static final String USER_ONE = "USER-ONE";
+    private static final String USER_TWO = "USER-TWO";
+
+    private DefaultPlanningPokerStorage defaultPlanningPokerStorage;
+
+    @Before
+    public void before() {
+        defaultPlanningPokerStorage = new DefaultPlanningPokerStorage();
+    }
+
+    @Test
+    public void cards_for_issue_hold_one_entry_per_user() {
+        // given
+        defaultPlanningPokerStorage.update(ISSUE_ONE, USER_ONE, "8");
+        defaultPlanningPokerStorage.update(ISSUE_ONE, USER_TWO, "5");
+
+        // when
+        defaultPlanningPokerStorage.update(ISSUE_ONE, USER_TWO, "8");
+
+        // then
+        assertThat(defaultPlanningPokerStorage.chosenCardsForIssue(ISSUE_ONE).size(), is(equalTo(2)));
+    }
+
+    @Test
+    public void cards_for_issue_are_not_visible_after_new_card() {
+        // given
+        defaultPlanningPokerStorage.update(ISSUE_ONE, USER_ONE, "8");
+        defaultPlanningPokerStorage.showDeck(ISSUE_ONE);
+        assertThat(defaultPlanningPokerStorage.isVisible(ISSUE_ONE), is(true));
+
+        // when
+        defaultPlanningPokerStorage.update(ISSUE_ONE, USER_ONE, "5");
+
+        // then
+        assertThat(defaultPlanningPokerStorage.isVisible(ISSUE_ONE), is(false));
+    }
+
+}
