db74a77d7dbf1056a53289819fd16596964ef583
Stefan Glase <stefan.glase@googlemail.com>
2015-04-24 10:46:43 +0200

    extract confirmation of estmations

--- src/main/java/net/congstar/jira/plugins/planningpoker/action/StartPlanningPoker.java
+++ src/main/java/net/congstar/jira/plugins/planningpoker/action/StartPlanningPoker.java
@@ -6,7 +6,6 @@ import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayout;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutManager;
-import com.atlassian.jira.issue.util.DefaultIssueChangeHolder;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.util.UserManager;
@@ -23,215 +22,205 @@ import java.util.*;
 
 public final class StartPlanningPoker extends JiraWebActionSupport {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final IssueManager issueManager;
+    private final IssueManager issueManager;
 
-	private final JiraAuthenticationContext context;
+    private final JiraAuthenticationContext context;
 
-	private final CustomFieldManager customFieldManager;
+    private final CustomFieldManager customFieldManager;
 
-	private final PluginSettingsFactory settingsFactory;
+    private final PluginSettingsFactory settingsFactory;
 
-	private final PlanningPokerStorage planningPokerStorage;
+    private final PlanningPokerStorage planningPokerStorage;
 
-	private String issueSummary;
+    private String issueSummary;
 
-	private Double issueStoryPoints;
+    private Double issueStoryPoints;
 
-	private String issueKey;
+    private String issueKey;
 
-	private String issueProjectName;
+    private String issueProjectName;
 
-	private String issueProjectKey;
+    private String issueProjectKey;
 
-	@HtmlSafe
-	public String getIssueDescription() {
-		MutableIssue issue = issueManager.getIssueObject(issueKey);
-		FieldLayout fieldLayout = fieldLayoutManager.getFieldLayout(issue);
-		FieldLayoutItem fieldLayoutItem = fieldLayout
-				.getFieldLayoutItem(IssueFieldConstants.DESCRIPTION);
-		String rendererType = (fieldLayoutItem != null) ? fieldLayoutItem
-				.getRendererType() : null;
-		return rendererManager.getRenderedContent(rendererType,
-				issue.getDescription(), issue.getIssueRenderContext());
-	}
+    @HtmlSafe
+    public String getIssueDescription() {
+        MutableIssue issue = issueManager.getIssueObject(issueKey);
+        FieldLayout fieldLayout = fieldLayoutManager.getFieldLayout(issue);
+        FieldLayoutItem fieldLayoutItem = fieldLayout
+                .getFieldLayoutItem(IssueFieldConstants.DESCRIPTION);
+        String rendererType = (fieldLayoutItem != null) ? fieldLayoutItem
+                .getRendererType() : null;
+        return rendererManager.getRenderedContent(rendererType,
+                issue.getDescription(), issue.getIssueRenderContext());
+    }
 
-	private Map<String, String> cardsForIssue;
+    private Map<String, String> cardsForIssue;
 
-	public Double getIssueStoryPoints() {
-		return issueStoryPoints;
-	}
+    public Double getIssueStoryPoints() {
+        return issueStoryPoints;
+    }
 
-	public String getIssueProjectKey() {
-		return issueProjectKey;
-	}
+    public String getIssueProjectKey() {
+        return issueProjectKey;
+    }
 
-	public String getIssueKey() {
-		return issueKey;
-	}
+    public String getIssueKey() {
+        return issueKey;
+    }
 
-	public String getIssueProjectName() {
-		return issueProjectName;
-	}
+    public String getIssueProjectName() {
+        return issueProjectName;
+    }
 
-	public String getIssueSummary() {
-		return issueSummary;
-	}
+    public String getIssueSummary() {
+        return issueSummary;
+    }
 
-	public boolean isDeckVisible() {
-		return planningPokerStorage.isVisible(issueKey);
-	}
+    public boolean isDeckVisible() {
+        return planningPokerStorage.isVisible(issueKey);
+    }
 
-	private PokerCard[] cards = { new PokerCard("q", "q.jpg", "q_.jpg"),
-			new PokerCard("0", "0.jpg", "0_.jpg"),
-			new PokerCard("1", "1.jpg", "1_.jpg"),
-			new PokerCard("2", "2.jpg", "2_.jpg"),
-			new PokerCard("3", "3.jpg", "3_.jpg"),
-			new PokerCard("5", "5.jpg", "5_.jpg"),
-			new PokerCard("8", "8.jpg", "8_.jpg"),
-			new PokerCard("13", "13.jpg", "13_.jpg"),
-			new PokerCard("20", "20.jpg", "20_.jpg"),
-			new PokerCard("40", "40.jpg", "40_.jpg"),
-			new PokerCard("100", "100.jpg", "100_.jpg") };
+    private PokerCard[] cards = {new PokerCard("q", "q.jpg", "q_.jpg"),
+            new PokerCard("0", "0.jpg", "0_.jpg"),
+            new PokerCard("1", "1.jpg", "1_.jpg"),
+            new PokerCard("2", "2.jpg", "2_.jpg"),
+            new PokerCard("3", "3.jpg", "3_.jpg"),
+            new PokerCard("5", "5.jpg", "5_.jpg"),
+            new PokerCard("8", "8.jpg", "8_.jpg"),
+            new PokerCard("13", "13.jpg", "13_.jpg"),
+            new PokerCard("20", "20.jpg", "20_.jpg"),
+            new PokerCard("40", "40.jpg", "40_.jpg"),
+            new PokerCard("100", "100.jpg", "100_.jpg")};
 
-	private Map<String, PokerCard> cardDeck = new HashMap<String, PokerCard>();
+    private Map<String, PokerCard> cardDeck = new HashMap<String, PokerCard>();
 
-	public Map<String, PokerCard> getCardDeck() {
-		return cardDeck;
-	}
+    public Map<String, PokerCard> getCardDeck() {
+        return cardDeck;
+    }
 
-	private String chosenCard;
+    private String chosenCard;
 
-	private FieldLayoutManager fieldLayoutManager;
+    private FieldLayoutManager fieldLayoutManager;
 
-	private RendererManager rendererManager;
+    private RendererManager rendererManager;
 
-	private UserManager userManager;
+    private UserManager userManager;
 
-	public String getChosenCard() {
-		return chosenCard;
-	}
+    public String getChosenCard() {
+        return chosenCard;
+    }
 
-	public PokerCard[] getCards() {
-		return cards;
-	}
+    public PokerCard[] getCards() {
+        return cards;
+    }
 
-	public StartPlanningPoker(IssueManager issueManager,
-			CustomFieldManager customFieldManager,
-			JiraAuthenticationContext context,
-			PluginSettingsFactory settingsFactory,
-			PlanningPokerStorage planningPokerStorage, UserManager userManager) {
-		this.issueManager = issueManager;
-		this.customFieldManager = customFieldManager;
-		this.context = context;
-		this.settingsFactory = settingsFactory;
-		this.planningPokerStorage = planningPokerStorage;
-		this.userManager = userManager;
+    public StartPlanningPoker(IssueManager issueManager,
+                              CustomFieldManager customFieldManager,
+                              JiraAuthenticationContext context,
+                              PluginSettingsFactory settingsFactory,
+                              PlanningPokerStorage planningPokerStorage, UserManager userManager) {
+        this.issueManager = issueManager;
+        this.customFieldManager = customFieldManager;
+        this.context = context;
+        this.settingsFactory = settingsFactory;
+        this.planningPokerStorage = planningPokerStorage;
+        this.userManager = userManager;
 
-		fieldLayoutManager = ComponentAccessor
-				.getComponent(FieldLayoutManager.class);
-		rendererManager = ComponentAccessor.getComponent(RendererManager.class);
-		for (PokerCard card : cards) {
-			cardDeck.put(card.getName(), card);
-		}
-	}
+        fieldLayoutManager = ComponentAccessor
+                .getComponent(FieldLayoutManager.class);
+        rendererManager = ComponentAccessor.getComponent(RendererManager.class);
+        for (PokerCard card : cards) {
+            cardDeck.put(card.getName(), card);
+        }
+    }
 
-	@Override
-	protected String doExecute() throws Exception {
-		HttpServletRequest request = ServletActionContext.getRequest();
-		issueKey = request.getParameter("issueKey");
+    @Override
+    protected String doExecute() throws Exception {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        issueKey = request.getParameter("issueKey");
 
-		ApplicationUser user = context.getUser();
-		if (user==null) 
-			return "error";
-		CustomField storyPointsField = findStoryPointField();
-		MutableIssue issue = issueManager.getIssueObject(issueKey);
+        ApplicationUser user = context.getUser();
+        if (user == null)
+            return "error";
+        CustomField storyPointsField = findStoryPointField();
+        MutableIssue issue = issueManager.getIssueObject(issueKey);
 
-		if (issue == null) {
-			addErrorMessage("Issue Key" + issueKey + " not found.");
-			return "error";
-		}
+        if (issue == null) {
+            addErrorMessage("Issue Key" + issueKey + " not found.");
+            return "error";
+        }
 
-		cardsForIssue = planningPokerStorage.chosenCardsForIssue(issueKey);
-		chosenCard = cardsForIssue.get(user.getKey());
+        cardsForIssue = planningPokerStorage.chosenCardsForIssue(issueKey);
+        chosenCard = cardsForIssue.get(user.getKey());
 
-		issueSummary = issue.getSummary();
-		issueProjectName = issue.getProjectObject().getName();
-		issueProjectKey = issue.getProjectObject().getKey();
-		issueStoryPoints = (Double) issue.getCustomFieldValue(storyPointsField);
+        issueSummary = issue.getSummary();
+        issueProjectName = issue.getProjectObject().getName();
+        issueProjectKey = issue.getProjectObject().getKey();
+        issueStoryPoints = (Double) issue.getCustomFieldValue(storyPointsField);
 
-		String finalVote = request.getParameter("finalVote");
-		if (finalVote != null) {
-			storyPointsField.updateValue(null, issue, new ModifiedValue(
-					issueStoryPoints, new Double(finalVote)),
-					new DefaultIssueChangeHolder());
-			getRedirect("/browse/" + issueKey);
-		}
+        return "start";
+    }
 
-		return "start";
-	}
+    private Set<Integer> getSortedVotes(Map<String, String> votes) {
+        Collection<String> votedValues = votes.values();
+        Set<Integer> uniqueValues = new TreeSet<Integer>();
+        for (String value : votedValues) {
+            if (!value.equals("q")) {
+                uniqueValues.add(new Integer(value));
+            }
+        }
+        return uniqueValues;
+    }
 
-	private Set<Integer> getSortedVotes(Map<String, String> votes) {
-		Collection<String> votedValues = votes.values();
-		Set<Integer> uniqueValues = new TreeSet<Integer>();
-		for (String value : votedValues) {
-			if (!value.equals("q")) {
-				uniqueValues.add(new Integer(value));
-			}
-		}
-		return uniqueValues;
-	}
+    public Collection<String> getBoundedVotes() {
+        ArrayList<String> votes = new ArrayList<String>();
+        for (Integer value : getSortedVotes(cardsForIssue)) {
+            votes.add(value.toString());
+        }
+        ArrayList<String> boundedVotes = new ArrayList<String>();
 
-	public Collection<String> getBoundedVotes() {
-		ArrayList<String> votes = new ArrayList<String>();
-		for (Integer value : getSortedVotes(cardsForIssue)) {
-			votes.add(value.toString());
-		}
-		ArrayList<String> boundedVotes = new ArrayList<String>();
+        String first = votes.get(0);
+        String last = votes.get(votes.size() - 1);
 
-		String first = votes.get(0);
-		String last = votes.get(votes.size() - 1);
+        if (votes.size() > 0) {
 
-		if (votes.size() > 0) {
+            int index = 0;
+            while (!cards[index].getName().equals(first)) {
+                index++;
+            }
+            boundedVotes.add(cards[index].getName());
+            if (votes.size() > 1) {
+                index++;
+                while (!cards[index].getName().equals(last)) {
+                    boundedVotes.add(cards[index].getName());
+                    index++;
+                }
+                boundedVotes.add(cards[index].getName());
+            }
+        }
+        return boundedVotes;
+    }
 
-			int index = 0;
-			while (!cards[index].getName().equals(first)) {
-				index++;
-			}
-			boundedVotes.add(cards[index].getName());
-			if (votes.size() > 1) {
-				index++;
-				while (!cards[index].getName().equals(last)) {
-					boundedVotes.add(cards[index].getName());
-					index++;
-				}
-				boundedVotes.add(cards[index].getName());
-			}
-		}
-		return boundedVotes;
-	}
+    private CustomField findStoryPointField() {
+        PluginSettings settings = settingsFactory.createGlobalSettings();
+        String field = (String) settings.get(ConfigurePlanningPokerAction.STORY_POINT_FIELD_NAME);
+        String storyPointFieldName = field == null ? "points" : field;
 
-	private CustomField findStoryPointField() {
-		PluginSettings settings = settingsFactory.createGlobalSettings();
-		String storyPointFieldName = settings
-				.get(ConfigurePlanningPokerAction.STORY_POINT_FIELD_NAME) != null ? (String) settings
-				.get(ConfigurePlanningPokerAction.STORY_POINT_FIELD_NAME) : "points";
-		List<CustomField> field = customFieldManager.getCustomFieldObjects();
-		for (CustomField customField : field) {
-			if (customField.getNameKey().toLowerCase()
-					.contains(storyPointFieldName.toLowerCase())) {
-				return customField;
-			}
-		}
-		return null;
-	}
+        for (CustomField customField : customFieldManager.getCustomFieldObjects()) {
+            if (customField.getNameKey().equalsIgnoreCase(storyPointFieldName)) {
+                return customField;
+            }
+        }
+        return null;
+    }
 
-	public Map<String, String> getCardsForIssue() {
-		return cardsForIssue;
-	}
+    public Map<String, String> getCardsForIssue() {
+        return cardsForIssue;
+    }
 
-	public String getUsername(String key) {
-		return userManager.getUserByKey(key).getDisplayName();
-	}
+    public String getUsername(String key) {
+        return userManager.getUserByKey(key).getDisplayName();
+    }
 }
