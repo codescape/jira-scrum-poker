bbbf21118945a163d9ab45f3832f46a0fbfdbe84
Stefan Glase <stefan.glase@googlemail.com>
2015-09-11 11:25:03 +0200

    reset planning poker after confirming an estimation

--- src/main/java/net/congstar/jira/plugins/scrumpoker/action/ConfirmEstimationAction.java
+++ src/main/java/net/congstar/jira/plugins/scrumpoker/action/ConfirmEstimationAction.java
@@ -1,5 +1,6 @@
 package net.congstar.jira.plugins.scrumpoker.action;
 
+import net.congstar.jira.plugins.scrumpoker.data.PlanningPokerStorage;
 import net.congstar.jira.plugins.scrumpoker.data.StoryPointFieldSupport;
 
 public class ConfirmEstimationAction extends ScrumPokerAction {
@@ -8,8 +9,11 @@ public class ConfirmEstimationAction extends ScrumPokerAction {
 
     private final StoryPointFieldSupport storyPointFieldSupport;
 
-    public ConfirmEstimationAction(StoryPointFieldSupport storyPointFieldSupport) {
+    private final PlanningPokerStorage planningPokerStorage;
+
+    public ConfirmEstimationAction(StoryPointFieldSupport storyPointFieldSupport, PlanningPokerStorage planningPokerStorage) {
         this.storyPointFieldSupport = storyPointFieldSupport;
+        this.planningPokerStorage = planningPokerStorage;
     }
 
     @Override
@@ -18,6 +22,7 @@ public class ConfirmEstimationAction extends ScrumPokerAction {
         String finalVote = getHttpRequest().getParameter("finalVote");
 
         storyPointFieldSupport.save(issueKey, new Double(finalVote));
+        planningPokerStorage.resetDeck(issueKey);
 
         String returnUrl = (String) getHttpSession().getAttribute(PARAM_RETURN_URL);
         getHttpSession().removeAttribute(PARAM_RETURN_URL);
