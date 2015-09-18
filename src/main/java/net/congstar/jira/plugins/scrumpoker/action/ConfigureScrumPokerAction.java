9596b14daa9fb24fa5ddd56940630ad8c7294063
Stefan Glase <stefan.glase@googlemail.com>
2015-09-18 15:35:01 +0200

    story point field name configuration now uses select list

--- src/main/java/net/congstar/jira/plugins/scrumpoker/action/ConfigureScrumPokerAction.java
+++ src/main/java/net/congstar/jira/plugins/scrumpoker/action/ConfigureScrumPokerAction.java
@@ -1,5 +1,10 @@
 package net.congstar.jira.plugins.scrumpoker.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 import com.atlassian.sal.api.pluginsettings.PluginSettings;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
@@ -13,10 +18,27 @@ public class ConfigureScrumPokerAction extends JiraWebActionSupport {
 
     private final PluginSettingsFactory settingsFactory;
 
-    public ConfigureScrumPokerAction(PluginSettingsFactory settingsFactory) {
+    private final CustomFieldManager customFieldManager;
+
+    public ConfigureScrumPokerAction(PluginSettingsFactory settingsFactory, CustomFieldManager customFieldManager) {
         this.settingsFactory = settingsFactory;
+        this.customFieldManager = customFieldManager;
     }
 
+    /**
+     * List of all custom field names currently available in this JIRA instance.
+     */
+    public List<String> getCustomFieldNames() {
+        List<String> customFieldNames = new ArrayList<String>();
+        for (CustomField customField : customFieldManager.getCustomFieldObjects()) {
+            customFieldNames.add(customField.getFieldName());
+        }
+        return customFieldNames;
+    }
+
+    /**
+     * Current configured name of the story point field in this JIRA instance. 
+     */
     public String getStoryPointFieldName() {
         PluginSettings settings = settingsFactory.createGlobalSettings();
         return (String) settings.get(STORY_POINT_FIELD_NAME);
@@ -24,9 +46,12 @@ public class ConfigureScrumPokerAction extends JiraWebActionSupport {
 
     @Override
     protected String doExecute() throws Exception {
-        PluginSettings settings = settingsFactory.createGlobalSettings();
-        String name = (String) settings.get(STORY_POINT_FIELD_NAME);
-        settings.put(STORY_POINT_FIELD_NAME, (name != null) ? name : DEFAULT_FIELD_FOR_STORY_POINTS);
+        String newStoryPointFieldName = getHttpRequest().getParameter(STORY_POINT_FIELD_NAME);
+        if (newStoryPointFieldName != null) {
+            PluginSettings settings = settingsFactory.createGlobalSettings();
+            settings.put(STORY_POINT_FIELD_NAME, newStoryPointFieldName);
+        }
+
         return "success";
     }
 
