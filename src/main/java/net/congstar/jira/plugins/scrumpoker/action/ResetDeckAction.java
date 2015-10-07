b920104653704520636c67b019662c37313f5cd5
Stefan Glase <stefan.glase@googlemail.com>
2015-10-07 13:51:20 +0200

    add docs

--- src/main/java/net/congstar/jira/plugins/scrumpoker/action/ResetDeckAction.java
+++ src/main/java/net/congstar/jira/plugins/scrumpoker/action/ResetDeckAction.java
@@ -2,6 +2,9 @@ package net.congstar.jira.plugins.scrumpoker.action;
 
 import net.congstar.jira.plugins.scrumpoker.data.PlanningPokerStorage;
 
+/**
+ * Start a new Scrum poker session and discard all cards previously presented by users.
+ */
 public class ResetDeckAction extends ScrumPokerAction {
 
     private static final long serialVersionUID = 1L;
