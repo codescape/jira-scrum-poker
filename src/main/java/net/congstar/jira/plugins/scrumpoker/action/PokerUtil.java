d40ca605f0734aaf038c5ab9895156334a3ed0da
Stefan Glase <stefan.glase@googlemail.com>
2015-11-02 21:13:22 +0100

    revert localized string #13

--- src/main/java/net/congstar/jira/plugins/scrumpoker/action/PokerUtil.java
+++ src/main/java/net/congstar/jira/plugins/scrumpoker/action/PokerUtil.java
@@ -4,7 +4,7 @@ import net.congstar.jira.plugins.scrumpoker.model.PokerCard;
 
 public class PokerUtil {
 
-    public static PokerCard[] pokerDeck = { new PokerCard("q", "q.jpg"), new PokerCard("0", "0.jpg"), new PokerCard("0,5", "05.jpg"), new PokerCard("1", "1.jpg"),
+    public static PokerCard[] pokerDeck = { new PokerCard("q", "q.jpg"), new PokerCard("0", "0.jpg"), new PokerCard("0.5", "05.jpg"), new PokerCard("1", "1.jpg"),
             new PokerCard("2", "2.jpg"), new PokerCard("3", "3.jpg"), new PokerCard("5", "5.jpg"), new PokerCard("8", "8.jpg"), new PokerCard("13", "13.jpg"),
             new PokerCard("20", "20.jpg"), new PokerCard("40", "40.jpg"), new PokerCard("100", "100.jpg") };
 
