632c53f815385a5873f9f0ea521d885363fe9c26
Stefan Glase <stefan.glase@googlemail.com>
2016-01-13 15:45:28 +0100

    fix typos and improve readability

--- src/test/java/net/congstar/jira/plugins/scrumpoker/data/DefaultPlanningPokerStorageTest.java
+++ src/test/java/net/congstar/jira/plugins/scrumpoker/data/DefaultPlanningPokerStorageTest.java
@@ -15,10 +15,11 @@ import org.junit.Test;
 
 public class DefaultPlanningPokerStorageTest {
 
+    private static final DateTime TWO_DAYS_AGO = DateTime.now().minusDays(2);
+    private static final DateTime FOURTEEN_HOURS_AGO = DateTime.now().minusHours(14);
+
     private static final String SOME_ISSUE = "Issue-1";
-
     private static final String YOUNG_ISSUE = "Young Issue";
-
     private static final String OLD_ISSUE = "Old Issue";
 
     private DefaultPlanningPokerStorage storage;
@@ -45,19 +46,18 @@ public class DefaultPlanningPokerStorageTest {
     }
 
     @Test
-    public void shoudlRemoveSessionsOlderThanOneDay() {
-        createOldScrumPokerSession(OLD_ISSUE, DateTime.now().minusDays(2).getMillis());
+    public void shouldRemoveSessionsOlderThanOneDay() {
+        createScrumPokerSessionWithCreationOn(OLD_ISSUE, TWO_DAYS_AGO.getMillis());
         assertThat(storage.sessionForIssue(OLD_ISSUE).getStartedOn(), is(equalTo(DateTime.now())));
     }
 
     @Test
-    public void shoudlKeepSessionsYoungerThanOneDay() {
-        DateTime youngEnough = DateTime.now().minusHours(14);
-        createOldScrumPokerSession(YOUNG_ISSUE, youngEnough.getMillis());
-        assertThat(storage.sessionForIssue(YOUNG_ISSUE).getStartedOn(), is(equalTo(youngEnough)));
+    public void shouldKeepSessionsYoungerThanOneDay() {
+        createScrumPokerSessionWithCreationOn(YOUNG_ISSUE, FOURTEEN_HOURS_AGO.getMillis());
+        assertThat(storage.sessionForIssue(YOUNG_ISSUE).getStartedOn(), is(equalTo(FOURTEEN_HOURS_AGO)));
     }
 
-    private void createOldScrumPokerSession(String issueKey, long creationDate) {
+    private void createScrumPokerSessionWithCreationOn(String issueKey, long creationDate) {
         DateTimeUtils.setCurrentMillisFixed(creationDate);
         storage.sessionForIssue(issueKey);
         DateTimeUtils.setCurrentMillisSystem();
