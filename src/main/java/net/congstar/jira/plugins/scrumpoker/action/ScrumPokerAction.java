33164ec3e74dd595947d109bd7a2def8420545ab
Stefan Glase <stefan.glase@googlemail.com>
2017-08-11 11:32:58 +0200

    change visibility

--- src/main/java/net/congstar/jira/plugins/scrumpoker/action/ScrumPokerAction.java
+++ src/main/java/net/congstar/jira/plugins/scrumpoker/action/ScrumPokerAction.java
@@ -2,14 +2,14 @@ package net.congstar.jira.plugins.scrumpoker.action;
 
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 
-public abstract class ScrumPokerAction extends JiraWebActionSupport {
+abstract class ScrumPokerAction extends JiraWebActionSupport {
 
     private static final long serialVersionUID = 1L;
 
     /**
      * Name of the parameter containing the issue key.
      */
-    protected static final String PARAM_ISSUE_KEY = "issueKey";
+    static final String PARAM_ISSUE_KEY = "issueKey";
 
     /**
      * Open the Scrum poker view for the given issue key.
@@ -17,7 +17,7 @@ public abstract class ScrumPokerAction extends JiraWebActionSupport {
      * @param issueKey issue key
      * @return redirect to Scrum poker view
      */
-    protected String openScrumPokerForIssue(String issueKey) {
+    String openScrumPokerForIssue(String issueKey) {
         return getRedirect("/secure/scrumPokerStart.jspa?issueKey=" + issueKey);
     }
 
@@ -27,7 +27,7 @@ public abstract class ScrumPokerAction extends JiraWebActionSupport {
      * @param issueKey issue key
      * @return redirect to detail url
      */
-    protected String openIssue(String issueKey) {
+    String openIssue(String issueKey) {
         return getRedirect("/browse/" + issueKey);
     }
 
