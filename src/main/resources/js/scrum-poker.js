a5a967b8a01a7ca06ad26e38e36840d479d7c788
Stefan Glase <stefan.glase@googlemail.com>
2018-03-26 20:42:15 +0200

    feat: after session is closed stop polling for update
    Users will be redirected to the issue page after 60 seconds if they are
    logged out or the Scrum Poker session has a confirmed vote.
    
    closes #4

--- src/main/resources/js/scrum-poker.js
+++ src/main/resources/js/scrum-poker.js
@@ -1,7 +1,11 @@
-/* Calling the refreshing of the Scrum Poker session every 2 seconds. */
+var scrumPokerRefresh;
+var scrumPokerRedirect;
+var scrumPokerRedirectScheduled = false;
+
+/* Calling the automatic refresh of the Scrum Poker session every 2 seconds. */
 function refreshOnInterval(issueKey) {
     refreshSession(issueKey);
-    setTimeout(function() {
+    scrumPokerRefresh = setTimeout(function() {
         refreshOnInterval(issueKey);
     }, 2000);
 }
@@ -12,7 +16,33 @@ function refreshSession(issueKey) {
         jQuery('#card-section').html(
             Mustache.render(jQuery('#card-section-template').html(), data)
         );
+        if(typeof data.confirmedVote !== 'undefined') {
+            scheduleRedirectToIssue(issueKey);
+        } else {
+            cancelRedirectToIssue();
+        }
+    }).fail(function() {
+        scheduleRedirectToIssue(issueKey);
     });
+}
+
+/* Schedule a redirect to the issue page after 60 seconds */
+function scheduleRedirectToIssue(issueKey) {
+    if (!scrumPokerRedirectScheduled) {
+        scrumPokerRedirect = setTimeout(function() {
+            clearTimeout(scrumPokerRefresh);
+            window.location.href = AJS.contextPath() + '/browse/' + issueKey;
+        }, 60000);
+        scrumPokerRedirectScheduled = true;
+    }
+}
+
+/* Cancel the redirect to the issue page */
+function cancelRedirectToIssue() {
+    if (scrumPokerRedirectScheduled) {
+        clearTimeout(scrumPokerRedirect);
+        scrumPokerRedirectScheduled = false;
+    }
 }
 
 /* Update Scrum Poker session and add or change a vote for the current user */
